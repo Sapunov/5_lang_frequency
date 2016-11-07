221d0f20c49fb2e5bd9e0cbfd50362e055238105
Nikita Sapunov <kiton1994@gmail.com>
2016-11-07 20:54:53 +0300

    New features: selected case-insensitive, count words

--- lang_frequency.py
+++ lang_frequency.py
@@ -15,14 +15,19 @@ def load_data(filepath):
         return input_file.read()
 
 
-def get_most_frequent_words(text, min_length=None):
+def extract_words(text, to_lowercase=False, min_length=None):
     words = re.findall(r"(\w+)", text, re.UNICODE)
 
-    words = map(lambda word: word.lower(), words)
+    if to_lowercase:
+        words = map(lambda word: word.lower(), words)
 
-    if min_length is not None:
+    if min_length:
         words = filter(lambda word: len(word) >= min_length, words)
 
+    return words
+
+
+def get_most_frequent_words(words):
     return Counter(words)
 
 
@@ -48,33 +53,49 @@ def main():
     parser.add_argument(
         "-t", "--tab",
         action="store_true",
-        help="TabSeparated format")
+        help="TabSeparated format"
+    )
+    parser.add_argument(
+        "-c", "--count",
+        action="store_true",
+        help="Show only number of words in text"
+    )
+    parser.add_argument(
+        "-i", "--ignore-case",
+        action="store_true",
+        help="Case insensitive"
+    )
 
     args = parser.parse_args()
 
     text = load_data(args.filepath)
-    most_frequent_words = get_most_frequent_words(text, args.min_length)
+    words = extract_words(text, args.ignore_case, args.min_length)
 
-    print_format = "{0:<6}{1:<25}{2}"
-
-    amount = args.n or None
-
-    if (args.tab):
-        for item in enumerate(most_frequent_words.most_common(amount)):
-            print("{0}\t{1}\t{2}".format(
-                item[0] + 1,
-                item[1][0],
-                item[1][1])
-            )
+    if args.count:
+        print(
+            len(list(words)))
     else:
-        print(print_format.format("#", "Word", "Freq"))
+        most_frequent_words = get_most_frequent_words(words)
+        amount = args.n or None
 
-        for item in enumerate(most_frequent_words.most_common(amount)):
-            print(print_format.format(
-                item[0] + 1,
-                item[1][0].ljust(25, "."),
-                item[1][1])
-            )
+        if args.tab:
+            for item in enumerate(most_frequent_words.most_common(amount)):
+                print("{0}\t{1}\t{2}".format(
+                    item[0] + 1,
+                    item[1][0],
+                    item[1][1])
+                )
+        else:
+            print_format = "{0:<6} {1:<25} {2}"
+
+            print(print_format.format("#", "Word", "Freq"))
+
+            for item in enumerate(most_frequent_words.most_common(amount)):
+                print(print_format.format(
+                    item[0] + 1,
+                    item[1][0].ljust(25, "."),
+                    item[1][1])
+                )
 
 
 if __name__ == "__main__":
